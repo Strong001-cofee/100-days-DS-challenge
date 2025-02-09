19b04ee8ef336c0fa2c770113ff825e13930a638
Strong Coffee <sahooanish436@gmail.com>
2025-02-09 15:24:03 +0530

    basic data types

---
+++ basic_data_types.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <cstdio>
+using namespace std;
+
+int main() {
+    int x;
+    long y;
+    char z;
+    float p;
+    double q;
+    cin >> x;
+    cin >> y;
+    cin >> z;
+    cin>>p;
+    cin >> q;
+    printf("%d\n%ld\n%c\n%f\n%lf ", x, y, z, p, q);
+
+    
+    return 0;
+}
